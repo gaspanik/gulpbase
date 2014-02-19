7ec42e060eab33d638cf0d402989d2df654e882d
cipher <gmgateway@gmail.com>
2014-02-19 10:03:41 +0900

    0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp')
+,	gutil = require('gulp-util')
 ,	jade = require('gulp-jade') // change your favorite HTML template engine. 
 ,	prettify = require('gulp-prettify')
 ,	less = require('gulp-less')
@@ -6,14 +7,20 @@ var gulp = require('gulp')
 ,	prefix = require('gulp-autoprefixer')
 ,	minifycss = require('gulp-minify-css')
 ,	rename = require('gulp-rename')
+,	imagemin = require('gulp-imagemin')
+,	changed = require('gulp-changed')
 ,	connect = require('gulp-connect');
 
+/*  Set your environment */
+
 var paths = {
+	"tplDir": "_templates/*.jade", // change your template extension.
 	"lessDir": "_less/*.less",
 	"scssDir": "_scss/*.scss",
-	"tplDir": "_templates/*.jade", // change your template extension.
 	"cssDir": "dist/css",
-	"destDir": "dist"
+	"imgDir": "_images/**",
+	"rootDir": "dist",
+	"imgsDir": "dist/images"
 }
 
 gulp.task('connect', connect.server({
@@ -25,45 +32,54 @@ gulp.task('connect', connect.server({
 );
 
 gulp.task('html', function() {
-	gulp.src(paths.tplDir)
-	.pipe(jade()) // change your template engine's function.
-	.pipe(gulp.dest(paths.destDir))
-	// If you need prettify HTML, comment below 2 lines.
-	// .pipe(prettify())
-	// .pipe(gulp.dest(paths.destDir))
-	.pipe(connect.reload());
+	return gulp.src(paths.tplDir)
+		.pipe(jade()) // change your template engine's function name.
+		.pipe(gulp.dest(paths.rootDir))
+		// If you would like to prettify HTML, uncomment below 2 lines.
+		// .pipe(prettify())
+		// .pipe(gulp.dest(paths.rootDir))
+		.pipe(connect.reload());
 });
 
 gulp.task('less', function() {
-	gulp.src(paths.lessDir)
-	.pipe(less())
-	.pipe(prefix('last 2 version'))
-	.pipe(gulp.dest(paths.cssDir))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(minifycss())
-	.pipe(gulp.dest(paths.cssDir))
-	.pipe(connect.reload());
+	return gulp.src(paths.lessDir)
+		.pipe(less())
+		.pipe(prefix('last 2 version'))
+		.pipe(gulp.dest(paths.cssDir))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest(paths.cssDir))
+		.pipe(connect.reload());
 });
 
 gulp.task('scss', function() {
-	gulp.src(paths.scssDir)
-	.pipe(sass({ style: 'expanded' }))
-	.pipe(prefix('last 2 version'))
-	.pipe(gulp.dest(paths.cssDir))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(minifycss())
-	.pipe(gulp.dest(paths.cssDir))
-	.pipe(connect.reload());
+	return gulp.src(paths.scssDir)
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(prefix('last 2 version'))
+		.pipe(gulp.dest(paths.cssDir))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest(paths.cssDir))
+		.pipe(connect.reload());
 });
 
-/*  If you would like to use Sass/SCSS, uncomment 'less' to 'scss'. */
+gulp.task('image', function() {
+	return gulp.src(paths.imgDir)
+		.pipe(changed(paths.imgsDir))
+		.pipe(imagemin({optimizationLevel: 3})) // See gulp-imagemin page.
+		.pipe(gulp.dest(paths.imgsDir));
+});
+
+
+// If you would like to use Sass/SCSS, uncomment 'less' to 'scss'.
 
 gulp.task('watch', function() {
 	gulp.watch([paths.tplDir], ['html']);
-	gulp.watch([paths.lessDir], ['less']);
-//	gulp.watch([paths.scssDir], ['scss']);
+	// gulp.watch([paths.lessDir], ['less']);
+	gulp.watch([paths.scssDir], ['scss']);
+	gulp.watch([paths.imgDir], ['image']);
 });
 
 // If you would like to use Sass/SCSS, rewrite 'less' to 'scss'.
 
-gulp.task('default', ['connect', 'html', 'less', 'watch']);
+gulp.task('default', ['image', 'connect', 'less', 'html', 'watch']);
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Gulpbase
 
 Sample gulpfile.js for HTML template (default: Jade) and CSS preprocessors (less or scss, default: less).   
-Auto-prefixer and Live-reload support.
+Auto-prefixer and Live-reload, Imagemin support.
 
 ## Usage
 
