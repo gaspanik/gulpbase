cf59208cb0922d6cdb8e493b1b1b3d0ce357c3fa
cipher <gmgateway@gmail.com>
2014-07-14 19:35:03 +0900

    0.0.5
    Change to Browser-Sync and Markdown support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,99 @@
-var gulp = require('gulp')
-,	gutil = require('gulp-util')
-,	jade = require('gulp-jade') // change your favorite HTML template engine.
-,	prettify = require('gulp-prettify')
-,	less = require('gulp-less')
-,	sass = require('gulp-ruby-sass')
-,	prefix = require('gulp-autoprefixer')
-,	minifycss = require('gulp-csso')
-,	rename = require('gulp-rename')
-,	imagemin = require('gulp-imagemin')
-,	changed = require('gulp-changed')
-,	connect = require('gulp-connect');
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  prefix = require('gulp-autoprefixer'),
+  $ = require('gulp-load-plugins')(),
+  browserSync = require('browser-sync');
 
 /*  Set your environment */
 
 var paths = {
-	"tplDir": "_templates/*.jade", // change your template extension.
-	"lessDir": "_less/*.less",
-	"scssDir": "_scss/*.scss",
-	"imgDir": "_images/**",
-	"rootDir": "dist",
-	"cssDir": "dist/css",
-	"imgsDir": "dist/images"
+  "tplDir": "_templates/*.jade", // change your template extension.
+  "lessDir": "_less/*.less",
+  "scssDir": "_scss/*.scss",
+  "imgDir": "_images/**",
+  "rootDir": "dist",
+  "cssDir": "dist/css",
+  "imgsDir": "dist/images"
 }
 
-gulp.task('connect', connect.server({
-	root: ['dist'],
-	port: 1337,
-	livereload: true,
-	open: {
-		"browser": "Duo" // {} set to blank, open default browser.
-		}
-	})
-);
+gulp.task('bs', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: paths.rootDir
+    },
+    notify: true,
+    xip: true
+  });
+});
 
 gulp.task('html', function() {
-	return gulp.src(paths.tplDir)
-		.pipe(jade()) // change your template engine's function name.
-		.pipe(gulp.dest(paths.rootDir))
-		// If you would like to prettify HTML, uncomment below 2 lines.
-		// .pipe(prettify())
-		// .pipe(gulp.dest(paths.rootDir))
-		.pipe(connect.reload());
+  return gulp.src(paths.tplDir)
+    .pipe($.jade())
+    .pipe(gulp.dest(paths.rootDir))
+  // If you need prettify HTML, uncomment below 2 lines.
+  // .pipe($.prettify())
+  // .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('less', function() {
-	return gulp.src(paths.lessDir)
-		.pipe(less({
-			sourceMap: true // If you don't need sourcemaps, set to false.
-		}))
-		.pipe(prefix('last 2 version'))
-		.pipe(gulp.dest(paths.cssDir))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest(paths.cssDir))
-		.pipe(connect.reload());
+  return gulp.src(paths.lessDir)
+    .pipe($.sourcemaps.init())
+    .pipe($.less())
+    .pipe(prefix('last 2 version'))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest(paths.cssDir))
+    .pipe($.rename({
+      suffix: '.min'
+    }))
+    .pipe($.csso())
+    .pipe(gulp.dest(paths.cssDir))
+    .pipe(browserSync.reload({
+      stream: true,
+      once: true
+    }));
 });
 
 gulp.task('scss', function() {
-	return gulp.src(paths.scssDir)
-		// If you need sourcemaps, pls. rewrite below options to {style: 'expanded' , sourcemap: true} .
-		// But you need to install sass 3.3 (gem install --pre sass)
-		// If you use bunde install, run 'bundle install --path yourpath' and rewrite option to {bundleExec: true, style: 'expanded' , sourcemap: true}.
-		.pipe(sass({ style: 'expanded' }))
-		.pipe(prefix('last 2 version'))
-		.pipe(gulp.dest(paths.cssDir))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest(paths.cssDir))
-		.pipe(connect.reload());
+  return gulp.src(paths.scssDir)
+    .pipe(sass({
+      style: 'expanded',
+      // sourcemap: true,
+      // sourcemapPath: paths.scssDir
+    }))
+    .pipe(prefix('last 2 version'))
+    .pipe(gulp.dest(paths.cssDir))
+    .pipe($.rename({
+      suffix: '.min'
+    }))
+    .pipe($.csso())
+    .pipe(gulp.dest(paths.cssDir))
+    .pipe(browserSync.reload({
+      stream: true,
+      once: true
+    }));
 });
 
 gulp.task('image', function() {
-	return gulp.src(paths.imgDir)
-		.pipe(changed(paths.imgsDir))
-		.pipe(imagemin({optimizationLevel: 3})) // See gulp-imagemin page.
-		.pipe(gulp.dest(paths.imgsDir));
+  return gulp.src(paths.imgDir)
+    .pipe($.changed(paths.imgsDir))
+    .pipe($.imagemin({
+      optimizationLevel: 3
+    })) // See gulp-imagemin page.
+  .pipe(gulp.dest(paths.imgsDir));
 });
 
 // If you would like to use Sass/SCSS, uncomment 'less' to 'scss'.
 
 gulp.task('watch', function() {
-	gulp.watch([paths.tplDir], ['html']);
-	gulp.watch([paths.lessDir], ['less']);
-	// gulp.watch([paths.scssDir], ['scss']);
-	gulp.watch([paths.imgDir], ['image']);
+  gulp.watch([paths.tplDir], ['html']);
+  gulp.watch([paths.lessDir], ['less']);
+  // gulp.watch([paths.scssDir], ['scss']);
+  gulp.watch([paths.imgDir], ['image']);
 });
 
 // If you would like to use Sass/SCSS, rewrite 'less' to 'scss'.
 
-gulp.task('default', ['image', 'connect', 'less', 'html', 'watch']);
+gulp.task('default', ['image', 'bs', 'less', 'html', 'watch']);
--- readme.md
+++ readme.md
@@ -1,19 +1,15 @@
 # Gulpbase
 
 Sample gulpfile.js for HTML template (default: Jade) and CSS preprocessors (less or scss, default: less).   
-Auto-prefixer and Live-reload, Imagemin support.
+Auto-prefixer and Browser-Sync, Imagemin support.
 
 ## Usage
 
-Install gulp plugins.
+Install gulpjs and some dependencies.
 
 	$ npm install
 
 Run gulp.
-
-	$ gulp
-
-If you use local install gulpjs, Run "npm start".
 
 	$ npm start
 
@@ -25,6 +21,6 @@ to
 
 > .pipe(prefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 
-## Chagelog 0.0.4
+## Chagelog 0.0.5
 
-* Minify CSS w/ csso.
+* Change to Browser-Sync. Markdown support.
